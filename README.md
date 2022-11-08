354a331bae99de8c1ec7cd4b5d9c6ed72cee8f13
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2022-11-07 18:36:22 -0600

    add meeting info

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Flask 101
 
 1. Estructura básica/setup
+```
+pip install flask
+```
 2. Rutas/métodos
+   - Siempre poner una función abajito de `@app.route`
+
 3. Returns
+   - Pueden ser strings o jsons
+   - Importar `jsonify` de flask
+
 4. Requests con body
+   - Importar `request` de flask
+
 5. Opciones de `run`
+   - Puedes poner `app.run(debug=True)` para poder editar el archivo sin tener que reiniciar el servidor
+   - Puedes cambiar el puerto con `app.run(port=5005)`
+
 6. Probar requests
+   - Usar postman
+   - También hay una extensión de VS Code: [Thunder Client](https://marketplace.visualstudio.com/items?itemName=rangav.vscode-thunder-client)
--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask('Flask tutorial')
 
+# Regresar un string
 @app.route('/', methods=['GET'])
 def home():
-    return 'hello world!'
+    return 'hello world'
 
-app.run()
+
+# regresar un json
+@app.route('/json-friend', methods=['GET'])
+def return_a_json():
+    hi = {
+        "id": "meep",
+        "message": "hello world"
+    }
+
+    return jsonify(hi)
+
+
+# leer el body que se pasa desde el request
+@app.route('/add-numbers', methods=['POST'])
+def do_something_with_body():
+    content = request.json
+
+    result = content[0] + content[1]
+
+    return jsonify(result)
+
+
+# correr con debug para que se recargue solito cuando haya cambios
+app.run(debug=True)
